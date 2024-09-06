82edc12951d06e17e65f6be60bb64df4b18adcde
Sosaia Sanft <tssanft92@gmail.com>
2024-09-06 00:08:15 -0700

    Completed US-05: Timer

--- src/index.js
+++ src/index.js
@@ -208,7 +208,10 @@ function clearScore() {
 */
 function updateTimer() {
   // TODO: Write your code here.
-  // hint: this code is provided to you in the instructions.
+  if (time > 0) {
+    time -= 1;
+    timerDisplay.textContent = time;
+  }
   
   return time;
 }
@@ -221,7 +224,8 @@ function updateTimer() {
 */
 function startTimer() {
   // TODO: Write your code here
-  // timer = setInterval(updateTimer, 1000);
+  timer = setInterval(updateTimer, 1000);
+
   return timer;
 }
 
@@ -249,7 +253,7 @@ function whack(event) {
 function setEventListeners(){
   // TODO: Write your code here
   rocks.forEach(rock => rock.addEventListener("click", whack));
-  
+
   return rocks;
 }
 
